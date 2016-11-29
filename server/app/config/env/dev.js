8f41fb283b418ea768cf88a7f9efed6368af2ba4
smikodanic <smikodanic@gmail.com>
2016-11-29 19:02:16 +0100

    config -dev

--- server/app/config/env/dev.js
+++ server/app/config/env/dev.js
@@ -17,13 +17,15 @@ var config = {
             {
                 name: 'supermeandev',
                 isActive: true, //true || false (if false then app will not use this mongodb)
-                uri: process.env.MONGODB_URI_1 || 'mongodb://supermean_user:smPass@127.0.0.1:27017/supermeandev',
+                // uri: process.env.MONGODB_URI_1 || 'mongodb://supermean_user:smPass@127.0.0.1:27017/supermeandev',
+                uri: process.env.MONGODB_URI_1 || 'mongodb://supermean_user:smPass@5.189.161.70:27017/supermean',
                 driver: 'mongoose'
             },
             {
                 name: 'supermeandev2',
                 isActive: false,
-                uri: process.env.MONGODB_URI_2 || 'mongodb://supermean_user:somePass@127.0.0.1:27017/supermeandev2',
+                // uri: process.env.MONGODB_URI_2 || 'mongodb://supermean_user:somePass@127.0.0.1:27017/supermeandev2',
+                uri: process.env.MONGODB_URI_2 || 'mongodb://supermean_user:somePass@5.189.161.70:27017/supermean2',
                 driver: 'mongoose'
             }
         ]
